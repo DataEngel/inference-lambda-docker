d930e5310827ba633fda317eccb14a74f3c9452f
DataEngel <miguel.program.73@gamil.com>
2022-06-04 16:36:02 -0500

    Update1

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,6 @@
 import boto3
 import warnings
-import os
+#import os
 
 from joblib import load 
 
@@ -12,13 +12,13 @@ def lambda_handler(event, context):
     payload = [[65, 0, 1, 10000, 1]] 
     
     AWS_S3_BUCKET = "engel-tests-20851"
-    ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-    SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+    #ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+    #SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
     
     s3_client = boto3.client(
-        "s3",
-        aws_access_key_id=ACCESS_KEY_ID,
-        aws_secret_access_key=SECRET_ACCESS_KEY,
+        "s3"
+        #aws_access_key_id=ACCESS_KEY_ID,
+        #aws_secret_access_key=SECRET_ACCESS_KEY,
     )
     filename = "model_risk.joblib"
     s3_client.download_file(AWS_S3_BUCKET, filename, '/tmp/' + filename)   
